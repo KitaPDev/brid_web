a67b3d4b0ea484986b235cce859f5b881d5a3795
Kita Pairojtanachai (Chris) <kita.pairojtanachai@gmail.com>
2022-05-05 10:45:42 -0400

    Revert "use https"
    This reverts commit 3d9026e7de7d2f52b07467370c01e427ec833ae3.

--- lib/http.ts
+++ lib/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `https://${process.env.NEXT_PUBLIC_ADDRESS}/api`,
+  baseURL: `http://${process.env.NEXT_PUBLIC_ADDRESS}/api`,
 });
 
 const http = {
