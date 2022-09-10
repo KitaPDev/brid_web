0152ee5ec43cfca52113862b04ab1a3e42731be9
Kati Pairojtanachai <kati.pairojtanachai@gmail.com>
2022-09-10 22:25:53 +0700

    added ICTI awards

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -8,6 +8,17 @@ interface NewsProps {
 
 function News({ news }: NewsProps) {
   const { t } = useTranslation("news");
+  const imageLinks = [
+    "https://scontent.fbkk5-7.fna.fbcdn.net/v/t39.30808-6/302753707_8167518656606618_7460009773524007020_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=JQE8J-vHP1cAX8SCHng&_nc_ht=scontent.fbkk5-7.fna&oh=00_AT_cPlXJvlo642H0moFQ8J31-cnY0-OK2nEDiPpChdPbWg&oe=632128B8",
+    "https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/302694001_8163269677031516_7678774886148039336_n.png?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=jsZsNehHI2cAX8n7q25&_nc_ht=scontent.fbkk5-3.fna&oh=00_AT-zyOZ3s2Im55ANUcy9fK7Zy_nFOWUKvu2m8o20Z2VhxA&oe=63214633",
+    "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/302133438_8163271153698035_8767198430308193120_n.png?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=Mpeem6Wo7oQAX_2tupE&tn=SDz7LmG5J1fVE413&_nc_ht=scontent.fbkk5-4.fna&oh=00_AT_DJcYwTgTRzjWhaO1yj-1JvkBfbmS8icLQZzEeyQwS4Q&oe=632131B0",
+    "https://scontent.fbkk5-8.fna.fbcdn.net/v/t39.30808-6/302420642_8163272570364560_7317796315709533174_n.png?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=bY04J3zFdQ4AX-uXYW_&_nc_ht=scontent.fbkk5-8.fna&oh=00_AT85YZz-MHky2o6qHBt0aglP4YzQJFOd9us8lxWm8ED_mg&oe=6320A498",
+    "https://scontent.fbkk5-6.fna.fbcdn.net/v/t39.30808-6/302513723_8163271230364694_7928103690621292316_n.png?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=S_5Ah6rwHdMAX8Ce1bb&tn=SDz7LmG5J1fVE413&_nc_ht=scontent.fbkk5-6.fna&oh=00_AT9CBbH2jS5392ASYsGkOSstB8wCXaUHM5Y7AQNrHp256w&oe=63222886",
+    "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/303608146_8168671189824698_592039730330882762_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=3RF-cQWIkOoAX83_ioK&_nc_ht=scontent.fbkk5-4.fna&oh=00_AT_HxrhLuVI-30CJjIyQ4k6efIXFVZC0rjWgfDErt4EPvQ&oe=63224AE4",
+    "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/303574254_8168671169824700_324852884747492797_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=Uw2p-b_q0UQAX8yihtS&_nc_oc=AQlJ4QMXKiU0kSW2dAN6w6bptWnCUNb6NPHEGfQkdXwTLN-1sh1XwH9YouwvY2mKFSc&_nc_ht=scontent.fbkk5-4.fna&oh=00_AT_LZJnd8QDzOprXBGLHWwze2XrABWniNh7pQOvFOPXeJg&oe=63223873",
+    "https://scontent.fbkk5-7.fna.fbcdn.net/v/t39.30808-6/302421022_8168671436491340_5240840426419622463_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=OsLAfkxmnlQAX96nZGc&_nc_ht=scontent.fbkk5-7.fna&oh=00_AT_OhYrWRKmE6c3iqGrwL_Tld5nkYmu_QyIfMXKR_iP6-w&oe=63212C36",
+    "https://scontent.fbkk5-6.fna.fbcdn.net/v/t39.30808-6/302430153_8168671433158007_7372746972604384874_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=ZNZ2445zbG4AX8Yw9Zm&_nc_ht=scontent.fbkk5-6.fna&oh=00_AT_p-kXZJJz3cGmSNx8x5WXQkqC7Bw_ZBUoKmoZxB001fQ&oe=6320DB2C"
+  ]
 
   return (
     <div>
@@ -115,6 +126,24 @@ function News({ news }: NewsProps) {
               ></iframe>
             </div>
             <p>{t("description3")}</p>
+          </div>
+          <div className="block-news">
+            <h3>{t("title5")}</h3>
+            <div className="grid grid-cols-1 gap-4 space-y-4 md:grid-cols-3 md:space-y-0">
+              {
+                imageLinks.map((val) => {
+                  return (
+                        <div
+                          className="max-h-full aspect-w-16 aspect-h-9 relative"
+                        >
+                          <img src={val} />
+                        </div>
+                      );
+                })
+            }
+            </div>
+            <p>{t("description5")}</p>
+            <a href="https://www.facebook.com/icticlub/posts/pfbid0zFSH6TFDprrxdZb6jzFNFvpamDCSdmSAAG4H4jJwcqABAM6NVXxnkw1TPqW5W2qel" target="_blank">{t("link_icti_post")}</a>
           </div>
         </div>
       </section>
